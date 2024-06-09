5a2264df93d1704cef4ee9534f677714752808f1
JuraQ8 <pavelelimov14122007@gmail.com  git config --global user.name JuraQ8git config --global user.email pavelelimov14122007@gmail.com>
2024-06-09 22:31:48 +0000

    Revert "Rewrite logging"
    This reverts commit 80b1a8d3e32e864d13560288efde36198a43e1a1.

--- __init__.py
+++ __init__.py
@@ -5,13 +5,14 @@ import importlib.util
 import psycopg2
 import sqlite3
 import database.db as db
+
 import discord
 import asyncio
 import colorama
 
 #from TOKEN import TOKEN # Раскомментируйте эту строку если вы используете TOKEN.py внутри рабочей директории
 from bot_params import PREFIX
-from modules.logger.commands import Logger
+from modules.logger.commands import error, info, warning, output
 from _functions_base import *
 from colorama import Fore, Style, Back
 from discord.ext import commands
@@ -25,10 +26,8 @@ import os
 match os.name:
     case "nt":
         __AUTH_FILE_PATH = os.path.dirname(os.getcwd()) + "\TOKEN.py"
-        NEWLINE = "\n"
     case "posix":
         __AUTH_FILE_PATH = os.path.dirname(os.getcwd()) + '/TOKEN.py'
-        NEWLINE = "\r"
 
 
 MAIN_COLOR = discord.Color.purple()
@@ -36,7 +35,6 @@ MAIN_COLOR = discord.Color.purple()
 colorama.init(autoreset=True)
 intents = discord.Intents.default()
 
-log = Logger("log.log")
 
 
 bot = commands.Bot(command_prefix=PREFIX, help_command=PrettyHelp(color=MAIN_COLOR, no_category="Technical Commands"), intents=discord.Intents.all())
